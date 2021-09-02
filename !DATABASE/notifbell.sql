6a3547d7aaa270c86416d6b9f275b346f079e1d8
AFsenna <Fgelicia@gmail.com>
2021-09-02 19:07:46 +0700

    fixed database (2)

--- !DATABASE/notifbell.sql
+++ !DATABASE/notifbell.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Agu 2021 pada 19.54
+-- Waktu pembuatan: 02 Sep 2021 pada 14.06
 -- Versi server: 10.1.40-MariaDB
 -- Versi PHP: 7.3.5
 
@@ -41,10 +41,8 @@ CREATE TABLE `aplikasi` (
 
 INSERT INTO `aplikasi` (`id_aplikasi`, `nama_aplikasi`, `user_id`, `status`) VALUES
 (1, 'Cabriz', 2, 1),
-(3, 'Si-Sembako', 23, 1),
-(6, 'Naest', 2, 1),
-(7, 'Tokopedia', 2, 0),
-(8, 'Cobayuk', 24, 1);
+(2, 'Naest', 2, 1),
+(3, 'sa', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -63,11 +61,11 @@ CREATE TABLE `detail_notifikasi` (
 --
 
 INSERT INTO `detail_notifikasi` (`pengguna_id`, `notifikasi_id`, `status`) VALUES
-(1, 1, 0),
-(2, 1, 0),
-(6, 1, 0),
-(7, 1, 0),
-(1, 2, 0);
+(1, 2, 1),
+(9, 2, 1),
+(10, 2, 1),
+(11, 2, 1),
+(12, 4, 0);
 
 -- --------------------------------------------------------
 
@@ -81,18 +79,19 @@ CREATE TABLE `notifikasi` (
   `judul` varchar(128) NOT NULL,
   `isi` longtext NOT NULL,
   `status` int(11) NOT NULL,
-  `tanggalDibuat` date NOT NULL
+  `tanggalDibuat` date NOT NULL,
+  `tanggalTerkirim` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `notifikasi`
 --
 
-INSERT INTO `notifikasi` (`id_notifikasi`, `aplikasi_id`, `judul`, `isi`, `status`, `tanggalDibuat`) VALUES
-(1, 1, 'testing notifikasi', '<p><b>semoga notifikasi ini terkirim</b></p>', 0, '2021-07-28'),
-(2, 1, 'tes', '<p>vfdbf</p>', 0, '2021-08-22'),
-(3, 6, 'pergantian nama aplikasi', '<p><b>selamat datang</b></p>', 0, '2021-08-22'),
-(4, 8, 'dor', '<p><b>doremi</b></p>', 1, '2021-08-22');
+INSERT INTO `notifikasi` (`id_notifikasi`, `aplikasi_id`, `judul`, `isi`, `status`, `tanggalDibuat`, `tanggalTerkirim`) VALUES
+(2, 1, 'testing', '<p><b>haloo</b></p>', 1, '2021-09-16', '2021-09-02'),
+(3, 1, 'Pemotongan Gaji', '<p>dassa</p>', 0, '2021-08-03', '0000-00-00'),
+(4, 2, 'Pembayaran Uang Kas', '<p><b>Selamat siang, </b></p><ul><li><u>diharapkan untuk segera membayar kas karena akan segera diadakan pembukuan</u></li></ul>', 0, '2021-09-02', '0000-00-00'),
+(5, 3, 'Pengumpulan Laporan PJK', 'Deadline pengumpulan PJK : Senin <br>\r\nHarap segera dikumpulkan!!!!!', 0, '2021-09-01', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -105,20 +104,22 @@ CREATE TABLE `pengguna_aplikasi` (
   `aplikasi_id` int(11) NOT NULL,
   `nama_pengguna` varchar(30) NOT NULL,
   `email_pengguna` varchar(30) NOT NULL,
-  `notelp_pengguna` varchar(18) NOT NULL
+  `notelp_pengguna` varchar(18) NOT NULL,
+  `status_pengguna` int(11) NOT NULL,
+  `tanggal_dibuat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `pengguna_aplikasi`
 --
 
-INSERT INTO `pengguna_aplikasi` (`id_pengguna`, `aplikasi_id`, `nama_pengguna`, `email_pengguna`, `notelp_pengguna`) VALUES
-(1, 1, 'Areta', 'tdngore@gmail.com', '0821424234'),
-(2, 1, 'adela', 'delaa@gmail.com', '08214224'),
-(3, 3, 'dyah', 'gajah@gmail.com', '0823266342'),
-(5, 3, 'tesupdate', 'gahupdate@gmail.com', '123456'),
-(6, 1, 'Senna', 'se9w@gmail.com', '+62822853122960'),
-(7, 1, 'dummy', 'dumsen08@gmail.com', '248621985');
+INSERT INTO `pengguna_aplikasi` (`id_pengguna`, `aplikasi_id`, `nama_pengguna`, `email_pengguna`, `notelp_pengguna`, `status_pengguna`, `tanggal_dibuat`) VALUES
+(1, 1, 'Senna', 'sagoliacais@gmail.com', '+62822853122960', 1, '2021-08-11'),
+(9, 1, 'Felicia Seanne', 'Fgelicia@gmail.com', '+62822853122960', 1, '2021-09-01'),
+(10, 1, 'Hoshi', 'horange@gmail.com', '089907212341', 1, '2021-09-02'),
+(11, 1, 'Woozi', 'woahe@gmail.com', '085156084242', 1, '2021-09-01'),
+(12, 2, 'Senna', 'senna@gmail.com', '085156084243', 1, '2021-09-01'),
+(13, 1, 'Senna', 'senna@gmail.com', '085156084243', 1, '2021-09-01');
 
 -- --------------------------------------------------------
 
@@ -174,10 +175,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama_lengkap`, `username`, `notelp`, `email`, `password`, `role_id`, `status`) VALUES
-(1, 'Admin Notifbell', 'senna', '082285132960', 'fgelicia@gmail.com', '$2y$10$NfGmvzYXp4c9XYJwX3RapuIDPGTdVFEtKNgRa/18KgBFX0BJP6Lyu', 1, 1),
-(2, 'Michael Araona Wily', 'Michael', '089912347653', 'araona@gmail.com', '$2y$10$dtpCI5WXuC3yhluu0S1Y0eF9HiNFDKfqHqooQqHAOmnf7MFDLTsy2', 2, 1),
-(23, 'Corrine Fadia Haya', 'Corrine', '0824563546', 'dumsen08@gmail.com', '$2y$10$lWzlfVOdFs6xoXvasSXC2u0yra3nOxxPJHbLR31jv9s2VLMEb9DFi', 2, 1),
-(24, 'Felicia Seanne', 'adminbasprog', '082266122960', 'michaelawek23@gmail.com', '$2y$10$Ln.BAMCW6fk/Mds4BFZ6Q.1UYt6BV0wczhY.Zv/bx67iI6x8FXoY.', 2, 1);
+(1, 'Admin Notifbell', 'senna', '082285132960', 'fgelicia@gmail.com', '$2y$10$kKAVgxSgiz4XMrxlvSPLVu7m.tAje1tXkB074JWp/BBx1uGdlkg9C', 1, 1),
+(2, 'Michael Araona Wily', 'Michael', '089912347653', 'araona@gmail.com', '$2y$10$dtpCI5WXuC3yhluu0S1Y0eF9HiNFDKfqHqooQqHAOmnf7MFDLTsy2', 2, 1);
 
 --
 -- Indexes for dumped tables
@@ -238,19 +237,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `aplikasi`
 --
 ALTER TABLE `aplikasi`
-  MODIFY `id_aplikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_aplikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `notifikasi`
 --
 ALTER TABLE `notifikasi`
-  MODIFY `id_notifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_notifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengguna_aplikasi`
 --
 ALTER TABLE `pengguna_aplikasi`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `role`
@@ -268,7 +267,7 @@ ALTER TABLE `token`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -291,7 +290,7 @@ ALTER TABLE `detail_notifikasi`
 -- Ketidakleluasaan untuk tabel `notifikasi`
 --
 ALTER TABLE `notifikasi`
-  ADD CONSTRAINT `notifikasi_ibfk_1` FOREIGN KEY (`aplikasi_id`) REFERENCES `aplikasi` (`id_aplikasi`);
+  ADD CONSTRAINT `notifikasi_ibfk_1` FOREIGN KEY (`aplikasi_id`) REFERENCES `aplikasi` (`id_aplikasi`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `pengguna_aplikasi`
