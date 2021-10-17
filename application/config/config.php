b7ad7de4938513d8e5362cf65d895ec79b7f9c07
Cyber Panel <awsen.id@gmail.com>
2021-10-17 15:39:30 +0000

    dariserver

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ManajemenNotifikasi/';
+$config['base_url'] = 'https://notifbell.awsen.my.id/';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,3 +521,4 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+$config['sess_save_path'] = '/tmp';
